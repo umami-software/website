a5ba526cdd0e640e6c15a95da40aca880ee4d594
Faraz Patankar <farazpatankar@gmail.com>
2021-04-29 11:33:33 +0400

    add running on railway guide

--- components/menu.js
+++ components/menu.js
@@ -25,6 +25,7 @@ const menu = {
   ],
   Guides: [
     ['Hosting', '/docs/hosting'],
+    ['Running on Railway', '/docs/running-on-railway'],
     ['Running on DigitalOcean', '/docs/running-on-digitalocean'],
     ['Running on Vercel', '/docs/running-on-vercel'],
     ['Running on Heroku', '/docs/running-on-heroku'],
