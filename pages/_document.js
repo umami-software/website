7034d4eda4693cbd6c027c5e300dd95e5ec04679
Mike Cao <mike@mikecao.com>
2022-03-16 21:32:45 -0700

    Move tracker script.

--- pages/_document.js
+++ pages/_document.js
@@ -10,14 +10,6 @@ export default function Document() {
           href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;600;900&display=swap"
           rel="stylesheet"
         />
-        {typeof window !== 'undefined' && process.env.NODE_ENV === 'production' && (
-          <script
-            async
-            defer
-            data-website-id="d0059975-b79a-4f83-8926-ed731475fded"
-            src="https://app.umami.is/script.js"
-          />
-        )}
       </Head>
       <body>
         <Main />
