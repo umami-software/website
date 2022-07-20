a51fe81b00747c4fe850fb8e2be3e0c21e4b7f11
Mike Cao <mike@mikecao.com>
2022-07-20 10:54:01 -0700

    CSS tweaks.

--- pages/community.js
+++ pages/community.js
@@ -3,11 +3,12 @@ import Community from 'components/Community';
 export default function CommunityPage() {
   return (
     <article className="container">
-      <header className="row justify-content-center">
+      <header>
         <h1>Community</h1>
-        <p className="col-12 col-sm-8">
-          Come join the Umami community of fellow developers, website owners, marketers and data
-          explorers. Get help with issues, discuss ideas and share your feedback.
+        <p>
+          Come join the Umami community of fellow developers, users and website owners.
+          <br />
+          Get help with issues, discuss ideas and share your feedback.
         </p>
       </header>
       <Community />
