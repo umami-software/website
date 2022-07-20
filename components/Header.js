bee465914d7b7fb2aa33bd441e9d8ac1729bfb82
Mike Cao <mike@mikecao.com>
2022-07-20 16:25:24 -0700

    Header styling.

--- components/Header.js
+++ components/Header.js
@@ -9,24 +9,26 @@ import HamburgerButton from './HamburgerButton';
 
 export default function Header() {
   return (
-    <header className={classNames('container', styles.header)}>
-      <div className={styles.title}>
-        <CompanyLogo />
+    <header>
+      <div className={classNames('container', styles.header)}>
+        <div className={styles.title}>
+          <CompanyLogo />
+        </div>
+        <div className={styles.links}>
+          <Link href="/features">Features</Link>
+          <Link href="/docs">Docs</Link>
+          <Link href="/community">Community</Link>
+          <Link href="/pricing">Pricing</Link>
+        </div>
+        <div className={styles.buttons}>
+          <Link href={REPO_URL}>
+            <a target="_blank">
+              <GitHub />
+            </a>
+          </Link>
+        </div>
+        <HamburgerButton />
       </div>
-      <div className={styles.links}>
-        <Link href="/features">Features</Link>
-        <Link href="/docs">Docs</Link>
-        <Link href="/community">Community</Link>
-        <Link href="/pricing">Pricing</Link>
-      </div>
-      <div className={styles.buttons}>
-        <Link href={REPO_URL}>
-          <a target="_blank">
-            <GitHub />
-          </a>
-        </Link>
-      </div>
-      <HamburgerButton />
     </header>
   );
 }
