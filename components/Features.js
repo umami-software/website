ca154878b7a730856acaffb76e431f672f374b2b
Mike Cao <mike@mikecao.com>
2022-08-16 00:39:41 -0700

    Fix images.

--- components/Features.js
+++ components/Features.js
@@ -6,53 +6,53 @@ const features = [
     name: 'Simple analytics',
     description:
       'Umami measures just the important metrics that you care about and everything is displayed on a single, easy to browse page.',
-    image: '/feature-website-stats.png',
+    image: '/images/feature-website-stats.png',
   },
   {
     name: 'Visitor insights',
     description: `Get detailed breakdowns about your visitors including what browser, OS and device they used.`,
-    image: '/feature-session-stats.png',
+    image: '/images/feature-session-stats.png',
   },
   {
     name: 'Custom events',
     description:
       'Track more than just pageviews. Capture any event on your website like button clicks and form entries.',
-    image: '/feature-events.png',
+    image: '/images/feature-events.png',
   },
   {
     name: 'Powerful filters',
     description:
       'Dive deeper into your data using easy to apply filters. Segment your users any metric such as browser, OS, and country.',
-    image: '/feature-filters.png',
+    image: '/images/feature-filters.png',
   },
   {
     name: 'Realtime data',
     description:
       'Get a realtime view of your current website traffic. See the exact pages where your visitors are landing.',
-    image: '/feature-realtime.png',
+    image: '/images/feature-realtime.png',
   },
   {
     name: 'Multi-language',
     description:
       'Umami has been translated into many different languages by the very active open source community.',
-    image: '/feature-languages.png',
+    image: '/images/feature-languages.png',
   },
   {
     name: 'Unlimited websites',
     description: `Umami is able to track an unlimited number of websites from a single installation. You can even track subdomains and individual URLs.`,
-    image: '/feature-websites.png',
+    image: '/images/feature-websites.png',
   },
   {
     name: 'Multiple accounts',
     description:
       'Umami can be used to host data for friends or clients. Just create a separate account and they can start tracking their own websites on their own dashboard.',
-    image: '/feature-accounts.png',
+    image: '/images/feature-accounts.png',
   },
   {
     name: 'Mobile-friendly',
     description:
       'The Umami interface has been optimized for mobile so you can view your stats from anywhere.',
-    image: '/feature-mobile.png',
+    image: '/images/feature-mobile.png',
   },
   {
     name: 'Bypass ad-blockers',
@@ -72,7 +72,7 @@ const features = [
   {
     name: 'Dark mode',
     description: 'Dark mode included. Choose your favorite theme for Umami.',
-    image: '/feature-dark-mode.png',
+    image: '/images/feature-dark-mode.png',
   },
 ];
 
