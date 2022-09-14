87770f137201d2ee05c81625f1ec38aa86652356
maxim <fedaraumaxim@gmail.com>
2022-09-14 12:52:43 +0300

    feat: sort imports and replace regular tags with the semantic ones

--- components/Header.js
+++ components/Header.js
@@ -1,34 +1,38 @@
+// Packages
 import Link from 'next/link';
 import classNames from 'classnames';
+
+// Own (components, utils, etc.)
+import CompanyLogo from 'components/CompanyLogo';
+import HamburgerButton from 'components/HamburgerButton';
 import { REPO_URL } from 'lib/constants';
-import CompanyLogo from './CompanyLogo';
 import GitHub from 'assets/github.svg';
 
-import styles from './Header.module.css';
-import HamburgerButton from './HamburgerButton';
+// Styles
+import styles from 'components/Header.module.css';
 
 export default function Header() {
   return (
     <header>
-      <div className={classNames('container', styles.header)}>
-        <div className={styles.title}>
+      <article className={classNames('container', styles.header)}>
+        <section className={styles.title}>
           <CompanyLogo />
-        </div>
-        <div className={styles.links}>
+        </section>
+        <nav className={styles.links}>
           <Link href="/features">Features</Link>
           <Link href="/docs">Docs</Link>
           <Link href="/community">Community</Link>
           <Link href="/pricing">Pricing</Link>
-        </div>
-        <div className={styles.buttons}>
+        </nav>
+        <section className={styles.buttons}>
           <Link href={REPO_URL}>
             <a target="_blank">
               <GitHub />
             </a>
           </Link>
-        </div>
+        </section>
         <HamburgerButton />
-      </div>
+      </article>
     </header>
   );
 }
