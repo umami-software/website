7354c25445200da7603bb918190bf2d3e71c2c0f
Mike Cao <mike@mikecao.com>
2023-04-14 05:51:42 -0700

    Pricing mockup.

--- components/Pricing.tsx
+++ components/Pricing.tsx
@@ -1,91 +1,220 @@
+import { Fragment } from 'react';
 import classNames from 'classnames';
-import { Button, Row, Column } from 'react-basics';
+import { Button, Row, Column, Icon } from 'react-basics';
 import Checkmark from 'assets/checkmark.svg';
-import Tools from 'assets/tools.svg';
-import Cloud from 'assets/cloud.svg';
 import styles from './Pricing.module.css';
 import Link from 'next/link';
+
+const features = [
+  {
+    label: 'Analytics',
+    items: [
+      ['Websites', 'Limited', true, true, true],
+      ['Teams', false, true, true, true],
+      ['Events', true, true, true, true],
+      ['Custom data', true, true, true, true],
+      ['Segments', false, true, true, true],
+      ['Goals', false, true, true, true],
+    ],
+  },
+  {
+    label: 'Advanced analysis',
+    items: [
+      ['API access', false, true, true, true],
+      ['A/B testing', false, true, true, true],
+      ['Heatmaps', false, false, true, true],
+      ['Session recording', false, false, true, true],
+      ['White label', false, false, true, true],
+      ['Forms', false, false, true, true],
+    ],
+  },
+  {
+    label: 'Reports',
+    items: [
+      ['Insights', true, true, true, true],
+      ['Flows', true, true, true, true],
+      ['Funnels', true, true, true, true],
+      ['Retention', true, true, true, true],
+      ['Cohorts', false, true, true, true],
+    ],
+  },
+  {
+    label: 'Monitoring',
+    items: [
+      ['Realtime events', true, true, true, true],
+      ['Custom dashboards', false, true, true, true],
+      ['Email alerts', false, false, true, true],
+    ],
+  },
+  {
+    label: 'Security',
+    items: [
+      ['Two-factor authentication', false, false, true, true],
+      ['Access controls', false, false, false, true],
+      ['SAML (SSO)', false, false, false, true],
+    ],
+  },
+  {
+    label: 'Support',
+    items: [
+      ['Email support', false, true, true, true],
+      ['Dedicated support', false, false, true, true],
+      ['Support SLAs', false, false, false, true],
+    ],
+  },
+];
 
 export default function Pricing() {
   return (
     <>
       <Row className={styles.pricing} justifyContent="center">
-        <Column xs={12} sm={12} md={6} defaultSize={4} className={classNames(styles.tier)}>
-          <div className={styles.title}>
-            <Tools /> Umami Open Source
+        <Column className={classNames(styles.tier)}>
+          <div className={styles.title}>Hobby</div>
+          <div className={styles.description}>Up to 10K monthly events</div>
+          <div className={styles.price}>
+            <b>Free</b>
           </div>
-          <div className={styles.description}>self-hosted</div>
-          <div className={styles.price}>Free</div>
           <ul>
             <li>
-              <Checkmark /> Unlimited websites
+              <Checkmark /> Up to 3 websites
             </li>
             <li>
-              <Checkmark /> Unlimited accounts
-            </li>
-            <li>
-              <Checkmark /> 100% data ownership
-            </li>
-            <li>
-              <Checkmark /> Your own infrastructure
+              <Checkmark /> 6 month data retention
             </li>
             <li>
               <Checkmark /> Community support
             </li>
           </ul>
           <div className={styles.button}>
-            <Link href="docs/getting-started">
+            <Link href="/pricing">
               <a>
                 <Button variant="secondary">Get Started</Button>
               </a>
             </Link>
           </div>
         </Column>
-        <Column
-          xs={12}
-          sm={12}
-          md={6}
-          defaultSize={4}
-          className={classNames(styles.tier, styles.cloud)}
-        >
-          <div className={styles.title}>
-            <Cloud /> Umami Cloud
+        <Column className={classNames(styles.tier)}>
+          <div className={styles.title}>Basic</div>
+          <div className={styles.description}>100K monthly events included</div>
+          <div className={styles.price}>
+            <b>$9</b> / month
           </div>
-          <div className={styles.description}>managed hosting</div>
-          <div className={styles.price}>Coming Soon</div>
           <ul>
             <li>
               <Checkmark /> Unlimited websites
             </li>
             <li>
-              <Checkmark /> Unlimited accounts
+              <Checkmark /> Unlimited team members
             </li>
             <li>
-              <Checkmark /> 100% data ownership
+              <Checkmark /> 2 year data retention
             </li>
             <li>
-              <Checkmark /> Fast global insfrastructure
+              <Checkmark /> Email support
+            </li>
+          </ul>
+          <div className={styles.button}>
+            <Link href="/pricing">
+              <a>
+                <Button variant="secondary">Get Started</Button>
+              </a>
+            </Link>
+          </div>
+        </Column>
+        <Column className={classNames(styles.tier)}>
+          <div className={styles.title}>Business</div>
+          <div className={styles.description}>1M monthly events included</div>
+          <div className={styles.price}>
+            <b>$49</b> / month
+          </div>
+          <ul>
+            <li>
+              <Checkmark /> Unlimited websites
+            </li>
+            <li>
+              <Checkmark /> Unlimited team members
+            </li>
+            <li>
+              <Checkmark /> 7 year data retention
             </li>
             <li>
               <Checkmark /> Dedicated support
             </li>
           </ul>
-        </Column>
-      </Row>
-      <Row justifyContent="center">
-        <Column className={styles.signup} defaultSize={6} xs={12} sm={12} md={8}>
-          <h2>Umami Cloud BETA</h2>
-          <p>Umami Cloud is currently running an open BETA program.</p>
-          <p>
-            You can try out all the features for <b>free</b> before we launch.
-          </p>
           <div className={styles.button}>
-            <Link href="https://cloud.umami.is/signup">
-              <a className="button" data-umami-event="cloud-beta-button" target="_blank">
-                <Button variant="primary">Sign up for Umami Cloud</Button>
+            <Link href="/pricing">
+              <a>
+                <Button variant="secondary">Get Started</Button>
               </a>
             </Link>
           </div>
+        </Column>
+        <Column className={classNames(styles.tier, styles.cloud)}>
+          <div className={styles.title}>Enterprise</div>
+          <div className={styles.description}>50M monthly events included</div>
+          <div className={styles.price}>
+            <b>$999</b> / month
+          </div>
+          <ul>
+            <li>
+              <Checkmark /> Unlimited websites
+            </li>
+            <li>
+              <Checkmark /> Unlimited team members
+            </li>
+            <li>
+              <Checkmark /> Custom data retention
+            </li>
+            <li>
+              <Checkmark /> Dedicated support & SLAs
+            </li>
+          </ul>
+          <div className={styles.button}>
+            <Link href="/pricing">
+              <a>
+                <Button variant="secondary">Contact Us</Button>
+              </a>
+            </Link>
+          </div>
+        </Column>
+      </Row>
+      <Row className={styles.features}>
+        <Column>
+          <table>
+            <tr>
+              <th></th>
+              <th>Hobby</th>
+              <th>Basic</th>
+              <th>Business</th>
+              <th>Enterprise</th>
+            </tr>
+            {features.map(({ label, items }) => {
+              return (
+                <Fragment key={label}>
+                  <h3>{label}</h3>
+                  {items.map((item, index) => {
+                    return (
+                      <tr key={index}>
+                        {item.map(data => {
+                          return (
+                            <td key={data}>
+                              {data === true ? (
+                                <Icon size="xl">
+                                  <Checkmark />
+                                </Icon>
+                              ) : (
+                                data
+                              )}
+                            </td>
+                          );
+                        })}
+                      </tr>
+                    );
+                  })}
+                </Fragment>
+              );
+            })}
+          </table>
         </Column>
       </Row>
     </>
