fe52d2e14ffc5193df85bdd79d67bcac413372d0
Mike Cao <mike@mikecao.com>
2023-07-14 00:41:43 -0700

    Pricing components. Added mobile support.

---
+++ components/pages/pricing/Calculator.tsx
@@ -0,0 +1,101 @@
+import { useState } from 'react';
+import { Radio, RadioGroup, Dropdown, Item } from 'react-basics';
+import styles from './Calculator.module.css';
+import classNames from 'classnames';
+
+const tiers = [
+  { price: 9, events: '100K' },
+  { price: 19, events: '250K' },
+  { price: 29, events: '500K' },
+  { price: 49, events: '1M' },
+  { price: 99, events: '2.5M' },
+  { price: 199, events: '5M' },
+  { price: 399, events: '10M' },
+  { price: 899, events: '25M' },
+  { price: 1699, events: '50M' },
+  { price: 2999, events: '100M' },
+  { price: 0, events: '100M+' },
+];
+
+export default function Calculator() {
+  const [tier, setTier] = useState(0);
+  const [billing, setBilling] = useState('Monthly');
+  const selected = tiers[tier];
+  const isAnnual = billing === 'Annual';
+
+  const handleBillingSelect = value => {
+    setBilling(value);
+  };
+
+  const handleTierSelect = value => {
+    setTier(value);
+  };
+
+  console.log('selectedKey', billing);
+
+  return (
+    <div className={styles.calculator}>
+      <div className={styles.input}>
+        <h1>Pro plan pricing</h1>
+        <div className={styles.toggle}>
+          <RadioGroup
+            name="billing"
+            items={['Monthly', 'Annual']}
+            selectedKey={billing}
+            onSelect={handleBillingSelect}
+            layout="horizontal"
+          >
+            {item => {
+              return <Radio key={item}>{item}</Radio>;
+            }}
+          </RadioGroup>
+        </div>
+        <div className={styles.slider}>
+          <input
+            className={styles.range}
+            type="range"
+            min="0"
+            max={tiers.length - 1}
+            value={tier}
+            onChange={e => handleTierSelect(e.target.value)}
+          />
+          <div className={styles.ticks}>
+            {tiers.map(({ events }, index) => (
+              <div
+                key={index}
+                className={classNames(styles.tick, { [styles.selected]: +tier === index })}
+                onClick={() => handleTierSelect(index)}
+              >
+                {events}
+              </div>
+            ))}
+          </div>
+        </div>
+        <div className={styles.select}>
+          <h3>Monthly events</h3>
+          <Dropdown
+            className={styles.dropdown}
+            items={tiers}
+            value={tiers[tier].events}
+            onChange={key => handleTierSelect(key)}
+          >
+            {({ events }, index) => <Item key={index}>{events}</Item>}
+          </Dropdown>
+        </div>
+        <div className={styles.result}>
+          <div className={styles.events}>
+            <strong>{selected.events}</strong> events / month
+          </div>
+          <div className={styles.price}>
+            <strong>
+              {selected.price > 0
+                ? `$${(selected.price * (isAnnual ? 10 : 1)).toLocaleString()}`
+                : 'Contact us'}
+            </strong>
+            {selected.price > 0 && <span>{` / ${isAnnual ? 'year' : 'month'}`}</span>}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
