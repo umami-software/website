9f48daaaff84c48356da662919d9a09d78c75fe7
Mike Cao <mike@mikecao.com>
2023-09-01 01:38:59 -0700

    Add missing script.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,6 +9,7 @@ import '@fontsource/jetbrains-mono/400.css';
 import 'react-basics/dist/styles.css';
 import 'styles/variables.css';
 import 'styles/index.css';
+import React from 'react';
 
 export default function RootLayout({ children }) {
   return (
@@ -22,6 +23,9 @@ export default function RootLayout({ children }) {
         <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
         <meta name="msapplication-TileColor" content="#da532c" />
         <meta name="theme-color" content="#ffffff" />
+        {process.env.NODE_ENV === 'production' && (
+          <script async data-website-id="86d4095c-a2a8-4fc8-9521-103e858e2b41" src="/a/script.js" />
+        )}
       </head>
       <body>
         <Header />
