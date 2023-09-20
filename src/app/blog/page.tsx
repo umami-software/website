15386d052283ce7dbc0c2773b691fd42d2541487
Mike Cao <mike@mikecao.com>
2023-09-19 18:59:04 -0700

    Added new sections.

---
+++ src/app/blog/page.tsx
@@ -0,0 +1,12 @@
+'use client';
+import PageHeader from 'components/layout/PageHeader';
+
+export default function BlogPage() {
+  return (
+    <article>
+      <PageHeader>
+        <h1>Blog</h1>
+      </PageHeader>
+    </article>
+  );
+}
