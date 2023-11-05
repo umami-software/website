0433920d58ffe74f86f9aef323c2e7982d7972be
Mike Cao <mike@mikecao.com>
2023-11-04 23:39:55 -0700

    Fixed links.

--- src/app/(website)/Footer.tsx
+++ src/app/(website)/Footer.tsx
@@ -26,7 +26,7 @@ const data = [
   {
     title: 'Community',
     items: [
-      { text: 'Get Involved', href: '/community' },
+      { text: 'Get Involved', href: '/developers' },
       { text: 'GitHub', href: 'https://github.com/umami-software/umami' },
       { text: 'Discord', href: 'https://discord.gg/4dz4zcXYrQ' },
       { text: 'Twitter', href: 'https://twitter.com/umami_software' },
