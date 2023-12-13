4b5829f4b132b534c3b775c48ed5f3c8a7cede5c
Mike Cao <mike@mikecao.com>
2023-12-13 01:42:17 -0800

    Added index page for cloud docs because Next is stupid.

---
+++ src/app/(website)/docs/cloud/Index.tsx
@@ -0,0 +1,6 @@
+'use client';
+import Page from './index.mdx';
+
+export default function Index() {
+  return <Page />;
+}
---
+++ src/app/(website)/docs/cloud/page.tsx
@@ -0,0 +1,5 @@
+import Index from './Index';
+
+export default function () {
+  return <Index />;
+}
