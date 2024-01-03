2d9d7334407ee78dbd2c957fcc1bd0f61ae24b98
Mike Cao <mike@mikecao.com>
2024-01-02 19:09:19 -0800

    Update page component for cloud docs.

--- src/app/(website)/docs/cloud/[id]/page.tsx
+++ src/app/(website)/docs/cloud/[id]/page.tsx
@@ -1,13 +1,17 @@
 import { glob } from 'glob';
-import PageContent from './PageContent';
 import { Metadata } from 'next';
+import NotFound from 'app/(website)/not-found';
+
+async function getContent(id: string) {
+  return import(`../${id}.mdx`).catch(() => ({}));
+}
 
 export async function generateMetadata({ params }: { params: { id: string } }): Promise<Metadata> {
-  const id = params.id;
+  const { id } = params;
 
-  return {
-    title: `${id[0].toUpperCase()}${id.slice(1).replace('-', ' ')}`.replace('.prefetch', ''),
-  };
+  const { meta } = await getContent(id);
+
+  return meta;
 }
 
 export async function generateStaticParams() {
@@ -18,8 +22,14 @@ export async function generateStaticParams() {
   }));
 }
 
-export default function ({ params }: { params: { id: string } }) {
-  const id = params?.id?.split('.')?.[0];
+export default async function ({ params }: { params: { id: string } }) {
+  const { id } = params;
 
-  return <PageContent id={id} />;
+  const { default: Page } = await getContent(id);
+
+  if (!Page) {
+    return <NotFound />;
+  }
+
+  return <Page />;
 }
