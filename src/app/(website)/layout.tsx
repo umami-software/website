6d1d632628ef984518e68d5b511b42f380d62f76
Francis Cao <franciscao@gmail.com>
2024-02-12 17:55:06 -0800

    fix layout for small devices

--- src/app/(website)/layout.tsx
+++ src/app/(website)/layout.tsx
@@ -15,6 +15,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en" data-scroll="0">
       <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <link rel="icon" href="/favicon.ico" />
         <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
