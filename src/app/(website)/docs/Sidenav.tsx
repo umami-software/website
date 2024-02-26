4c55fa0f54ae4622af84c6a71841c665a32080cd
Mike Cao <mike@mikecao.com>
2024-02-26 14:46:32 -0800

    Created docs menu for mobile.

--- src/app/(website)/docs/Sidenav.tsx
+++ src/app/(website)/docs/Sidenav.tsx
@@ -1,5 +1,7 @@
 'use client';
+import { useState } from 'react';
 import { usePathname } from 'next/navigation';
+import { Icons, Icon, Text } from 'react-basics';
 import classNames from 'classnames';
 import Menu from './Menu';
 import styles from './Sidenav.module.css';
@@ -7,18 +9,27 @@ import styles from './Sidenav.module.css';
 export default function Sidenav() {
   const pathname = usePathname();
   const isCloud = pathname.includes('/docs/cloud');
+  const [show, setShow] = useState(false);
 
   return (
     <div className={styles.sidenav}>
-      <div className={styles.toggle}>
-        <a href="/docs" className={classNames({ [styles.active]: !isCloud })}>
-          Self-host
-        </a>
-        <a href="/docs/cloud" className={classNames({ [styles.active]: isCloud })}>
-          Cloud
-        </a>
+      <div className={styles.button} onClick={() => setShow(!show)}>
+        <Text>Menu</Text>
+        <Icon>
+          <Icons.ChevronDown />
+        </Icon>
       </div>
-      <Menu />
+      <div className={classNames(styles.menu, { [styles.show]: show })}>
+        <div className={styles.toggle}>
+          <a href="/docs" className={classNames({ [styles.active]: !isCloud })}>
+            Self-host
+          </a>
+          <a href="/docs/cloud" className={classNames({ [styles.active]: isCloud })}>
+            Cloud
+          </a>
+        </div>
+        <Menu onClick={() => setShow(false)} />
+      </div>
     </div>
   );
 }
