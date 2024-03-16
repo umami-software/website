8f8655c2352027e76883b6b2b6cd4dce851305e7
Mike Cao <moocao@gmail.com>
2024-03-15 23:21:49 -0700

    Added companies section.

---
+++ src/app/(website)/Companies.tsx
@@ -0,0 +1,27 @@
+'use client';
+import TextBlock from 'components/layout/TextBlock';
+import styles from './Companies.module.css';
+
+const items = [
+  { name: 'amd', title: 'AMD' },
+  { name: 'accenture', title: 'Accenture' },
+  //{ name: 'bp', title: 'bp' },
+  { name: 'espn', title: 'ESPN' },
+  { name: 'siemens', title: 'Siemens' },
+  { name: 'hulu', title: 'Hulu' },
+  { name: 'vsp', title: 'VSP' },
+  //{ name: 'huawei', title: 'Huawei' },
+];
+
+export default function Companies() {
+  return (
+    <TextBlock align="center">
+      <h1>Trusted by thousands of companies</h1>
+      <div className={styles.logos}>
+        {items.map(({ name, title }) => {
+          return <img key={name} src={`/images/logo-${name}.png`} alt={title} />;
+        })}
+      </div>
+    </TextBlock>
+  );
+}
--- src/app/(website)/page.tsx
+++ src/app/(website)/page.tsx
@@ -7,14 +7,16 @@ import QuickStart from './QuickStart';
 import Quotes from './Quotes';
 import Features from './Features';
 import Personas from './Personas';
-import styles from './page.module.css';
 import OpenSource from 'app/(website)/OpenSource';
+import Companies from 'app/(website)/Companies';
+import styles from './page.module.css';
 
 export default function () {
   return (
     <article className={styles.container}>
       <Hero />
       <Statements />
+      <Companies />
       <Features />
       <QuickStart />
       <Personas />
