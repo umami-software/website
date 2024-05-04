03e0c4a3853618461313994fad92f8ed6a189cb4
Mike Cao <moocao@gmail.com>
2024-05-03 17:10:23 -0700

    Publish blog post.

--- src/app/(website)/blog/[id]/page.tsx
+++ src/app/(website)/blog/[id]/page.tsx
@@ -26,11 +26,17 @@ export default async function ({ params }: Props) {
   const { id } = params;
   const post = await getPost(id);
 
+  if (!post) {
+    return null;
+  }
+
   return (
     <article className={styles.blog}>
       <div className={styles.title}>{post?.title}</div>
       <div className={styles.info}>
-        <div className={styles.date}>{format(new Date(post?.date as string), 'PP')}</div>
+        <div className={styles.date}>
+          {post?.date && format(new Date(post?.date as string), 'PP')}
+        </div>
         <div className={styles.author}>Posted by {post?.author}</div>
       </div>
       <Markdown>{post?.body}</Markdown>
