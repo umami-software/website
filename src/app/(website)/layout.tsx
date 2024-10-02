9384790b9e9e262db2589ac6a1bab1f26a91d292
Francis Cao <franciscao@gmail.com>
2024-10-02 10:37:55 -0700

    fix import typos

--- src/app/(website)/layout.tsx
+++ src/app/(website)/layout.tsx
@@ -26,6 +26,11 @@ export default function RootLayout({ children }) {
         <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
         <meta name="msapplication-TileColor" content="#da532c" />
         <meta name="theme-color" content="#ffffff" />
+        <script
+          defer
+          src="https://cloud.umami.is/script.js"
+          data-website-id="712938ae-f6ba-4575-9b23-8aaa99aeb121"
+        ></script>
         {process.env.NODE_ENV === 'production' && (
           <script
             defer
