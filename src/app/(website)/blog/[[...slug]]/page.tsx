e9c219f16c0236876094ecf460ebacfa8fbabdce
Mike Cao <moocao@gmail.com>
2025-01-17 22:33:44 -0800

    Added page for blogs.

--- src/app/(website)/blog/[[...slug]]/page.tsx
+++ src/app/(website)/blog/[[...slug]]/page.tsx
@@ -1,22 +1,11 @@
 import config from '@/app/shiso.config.json';
 import { next } from '@umami/shiso/server';
 import { Shiso } from '@umami/shiso';
-import PageHeader from '@/components/PageHeader';
 
 const { generateMetadata, generateStaticParams, renderPage } = next('blog', config);
 
 export { generateMetadata, generateStaticParams };
 
 export default renderPage(props => {
-  return (
-    <>
-      {Array.isArray(props?.content) && (
-        <PageHeader
-          title="Blog"
-          description="Sharing our thoughts on analytics, marketing and open source"
-        />
-      )}
-      <Shiso {...props} />
-    </>
-  );
+  return <Shiso {...props} />;
 });
