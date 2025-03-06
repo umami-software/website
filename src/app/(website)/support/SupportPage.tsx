a4df2dc5189f900a570fe4a2a4d6c6ae28c25285
Mike Cao <moocao@gmail.com>
2025-03-06 08:45:10 -0800

    Updated stats.

--- src/app/(website)/support/SupportPage.tsx
+++ src/app/(website)/support/SupportPage.tsx
@@ -4,10 +4,7 @@ import PageHeader from '@/components/PageHeader';
 export default function SupportPage() {
   return (
     <section>
-      <PageHeader>
-        <h1>Support</h1>
-        <p>Support services available for Umami Cloud</p>
-      </PageHeader>
+      <PageHeader title="Support" description="Support services available for Umami Cloud" />
       <div style={{ textAlign: 'center' }}>
         <h2>
           <a href="mailto:support@umami.is">support@umami.is</a>
