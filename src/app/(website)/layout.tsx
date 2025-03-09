3783d9e767f655b912c4cf0dfa2d46383fc99a7d
Mike Cao <mike@mikecao.com>
2025-03-08 21:26:34 -0800

    Updated docs packages.

--- src/app/(website)/layout.tsx
+++ src/app/(website)/layout.tsx
@@ -11,7 +11,8 @@ import '@fontsource/inter/600.css';
 import '@fontsource/inter/700.css';
 import '@fontsource/jetbrains-mono/400.css';
 import '@umami/react-zen/styles.css';
-import '@umami/shiso/dist/index.css';
+import '@umami/shiso/styles.css';
+import 'highlight.js/styles/github-dark.css';
 import '@/styles/variables.css';
 import '@/styles/global.css';
 
@@ -37,7 +38,7 @@ export default function RootLayout({ children }) {
           />
         )}
       </head>
-      <body>
+      <body suppressHydrationWarning>
         <Suspense>
           <Grid rows="auto 1fr auto" height="100vh">
             <Header />
