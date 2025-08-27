1316b4131604d9479d2e71013b42119ac25651f4
Mike Cao <moocao@gmail.com>
2025-08-27 15:47:57 -0700

    Updated counts.

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -3,9 +3,9 @@ export const GITHUB_URL = 'https://github.com/umami-software/umami';
 export const DISCORD_URL = 'https://umami.is/discord';
 export const X_URL = 'https://twitter.com/umami_software';
 export const LINKEDIN_URL = 'https://www.linkedin.com/company/umami-software/';
-export const GITHUB_STARS = '26K';
+export const GITHUB_STARS = '30K';
 export const GITHUB_CONTRIBUTORS = '280';
-export const GITHUB_DOWNLOADS = '17M';
+export const GITHUB_DOWNLOADS = '20M';
 export const GITHUB_STATS = [
   { label: 'Downloads', value: GITHUB_DOWNLOADS },
   { label: 'GitHub stars', value: GITHUB_STARS },
